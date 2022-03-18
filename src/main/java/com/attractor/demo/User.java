55ac1aa8adf8efccb09cbc46d33d358e90509e4a
IgorPriadein <iga2456@gmail.com>
2022-03-18 18:58:08 +0600

    Part 1 - Created class User with its dependencies.

--- src/main/java/com/attractor/demo/User.java
+++ src/main/java/com/attractor/demo/User.java
@@ -1,4 +1,21 @@
 package com.attractor.demo;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class User {
+    private String username;
+    private String email;
+    private String password;
+    private Integer postCounter;
+    private Integer subscribersCounter;
+    private Integer subscriptionsCounter;
 }
